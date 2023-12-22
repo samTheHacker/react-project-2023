bdefab32aeffeed55cebb079348eb5b4a57b557d
Sammy D <mobilvodoo@googlemail.com>
2023-12-22 12:54:46 +0000

    Update App.js render props

--- src/App.js
+++ src/App.js
@@ -1,70 +1,43 @@
 import './App.css';
 import  {useState, useEffect} from "react";
 
-const query = `
-query {
-  allLifts {
-    name
-    elevationGain
-    status
-  }
+const tahoe_peaks = [
+  { name: "Freel", elevation: 10891 },
+  { name: "Monument", elevation: 10067 },
+  { name: "Pyramid", elevation: 9983 },
+  { name: "Tallac", elevation: 9735 }
+];
+
+function List({ data, renderItem, renderEmpty }) {
+  return !data.length ? (
+    renderEmpty
+  ) : (
+    <ul>
+      {data.map((item) => (
+        <li key={item.name}>
+          {renderItem(item)}
+        </li>
+      ))}
+    </ul>
+  ); //if empty return renderEmpty, else do the data.map
 }
-`;
 
-const opts = {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ query }) //pass it the body JSON
-};
-
-
-function Lift({ name, elevationGain, status }) {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <p>
-        {elevationGain} {status}
-      </p>
-    </div>
-  );
-}
 
 
 function App() {
 
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    fetch(
-      `https://snowtooth.moonhighway.com/`,
-      opts
-    )
-    .then((response) => response.json())
-    .then(setData)
-    .then(() => setLoading(false))
-    .catch(setError);
-  }, [] //call rest API once
-  ); 
-
-  if (loading) return <h1>Loading...</h1>
-  if(error)
-    return <pre>{JSON.stringify(error)}</pre>
-  if (!data) return null;
   return (
-    <div>
-      {data.data.allLifts.map((lift) => (
-        <Lift
-          name={lift.name}
-          elevationGain={lift.elevationGain}
-          status={lift.status}
-        />
-      ))}
-    </div>
+    <List
+      data={tahoe_peaks}
+      renderEmpty={<p>This list is empty</p>}
+      renderItem={(item) => (
+        <>
+          {item.name} - {item.elevation} ft.
+        </> //fragment
+      )}
+    />
   );
-
+  
 }
 
 export default App;
