8b707d534fca55b7908c3cd66b5b9b6400050141
Sammy D <mobilvodoo@googlemail.com>
2023-12-22 13:33:17 +0000

    Update App.js Link component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function Home(){
- return ( <div>
+ return ( 
+  <div>
+    <nav>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
     <h1>My Website</h1>
   </div>
  )
 }
 
 export function Contact(){
-  return (<div>
+  return (
+  <div>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
     <h1>Contact</h1>
   </div>
   )
 }
 
 export function About(){
-  return (<div>
+  return (
+  <div>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
     <h1>About me</h1>
   </div>
   )
