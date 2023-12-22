c55b94cd0fe0ec544819c6f4e51a1d4beda80426
Sammy D <mobilvodoo@googlemail.com>
2023-12-22 12:31:42 +0000

    Update App.js load states

--- src/App.js
+++ src/App.js
@@ -15,24 +15,31 @@ function GithubUser({name, location, avatar}){
 function App() {
 
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     fetch(`https://api.github.com/users/samTheHacker`)
     .then((response) => response.json())
-    .then(setData);
-  }, 
-  [] //call rest API once
+    .then(setData)
+    .then(() => setLoading(false))
+    .catch(setError);
+  }, [] //call rest API once
   ); 
 
-  if(data)
-    return ( 
+  if (loading) return <h1>Loading...</h1>
+  if(error)
+    return <pre>{JSON.stringify(error)}</pre>
+  if (!data) return null;
+  return ( 
       <GithubUser 
         name={data.name} 
         location={data.location} 
         avatar={data.avatar_url}
       />
-    );
-  return <h1>Data</h1>;
+  )
+
 }
 
 export default App;
