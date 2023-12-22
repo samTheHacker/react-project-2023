f6dd4a3d9fcd6abb0e83c21d34823a8046bf1594
Sammy D <mobilvodoo@googlemail.com>
2023-12-22 12:41:32 +0000

    Update App.js GraphQL logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import './App.css';
 import  {useState, useEffect} from "react";
 
-function GithubUser({name, location, avatar}){
-  return(
-  <div>
-    <h1>{name}</h1>
-    <p>{location}</p>
-    <img src={avatar} alt={name}/>
-  </div>
+const query = `
+query {
+  allLifts {
+    name
+    elevationGain
+    status
+  }
+}
+`;
+
+const opts = {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ query }) //pass it the body JSON
+};
+
+
+function Lift({ name, elevationGain, status }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>
+        {elevationGain} {status}
+      </p>
+    </div>
   );
 }
 
@@ -20,7 +38,10 @@ function App() {
 
   useEffect(() => {
     setLoading(true);
-    fetch(`https://api.github.com/users/samTheHacker`)
+    fetch(
+      `https://snowtooth.moonhighway.com/`,
+      opts
+    )
     .then((response) => response.json())
     .then(setData)
     .then(() => setLoading(false))
@@ -32,13 +53,17 @@ function App() {
   if(error)
     return <pre>{JSON.stringify(error)}</pre>
   if (!data) return null;
-  return ( 
-      <GithubUser 
-        name={data.name} 
-        location={data.location} 
-        avatar={data.avatar_url}
-      />
-  )
+  return (
+    <div>
+      {data.data.allLifts.map((lift) => (
+        <Lift
+          name={lift.name}
+          elevationGain={lift.elevationGain}
+          status={lift.status}
+        />
+      ))}
+    </div>
+  );
 
 }
 
